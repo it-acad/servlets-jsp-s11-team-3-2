e763f73db93dab4d3a635fdaa37d0fcc2d30a4bd
VovaProtsiv <vovapro111@gmail.com>
2021-01-05 15:33:32 +0200

    Created ReadTaskServlet

---
+++ src/main/java/com/softserve/itacademy/controller/ReadTaskServlet.java
@@ -0,0 +1,34 @@
+package com.softserve.itacademy.controller;
+
+import com.softserve.itacademy.model.Task;
+import com.softserve.itacademy.repository.TaskRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/read-task")
+public class ReadTaskServlet extends HttpServlet {
+
+    private TaskRepository repo;
+
+    @Override
+    public void init() {
+        repo = TaskRepository.getTaskRepository();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Task task = repo.read(Integer.parseInt(req.getParameter("id")));
+        if (task != null) {
+            req.setAttribute("task", task);
+            req.getRequestDispatcher("/WEB-INF/read-task.jsp").forward(req, resp);
+        } else {
+            resp.setStatus(404);
+            req.getRequestDispatcher("/WEB-INF/not-found-task.jsp").forward(req, resp);
+        }
+    }
+}
